99e9a7a0a5c0aa862e5188798cf0043c25d82fb4
Raphael Simonetti <raphasimonettti@gmail.com>
2023-09-11 03:22:07 -0300

    Calculando Quais Números São Pares No Array

---
+++ EvenNumbers/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		System.out.println("Vamos verificar quais numeros que você digitar vão ser pares");
+		int n = sc.nextInt();
+		int v[] = new int[n];
+
+		System.out.println("Digite os números");
+		
+		for (int i = 0; i < n; i++) {
+			v[i] = sc.nextInt();
+		}
+		int result = 0;
+		System.out.println("Números Pares");
+		Boolean evenOrnot = false;
+		
+	    for (int i=0; i<n; i++) {
+	        if (v[i] % 2 == 0) {
+	        	System.out.printf("%d  ", v[i]);
+	            result++;
+	            evenOrnot = true;
+	        }
+	    }
+	    if(evenOrnot == false) 
+	    {
+	    	System.out.println("Não existem números pares");
+	    }
+		
+		
+	}
+
+}

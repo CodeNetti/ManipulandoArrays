bed37ee895ccd99fb1a4269108daf0ab99cc666d
Raphael Simonetti <raphasimonettti@gmail.com>
2023-09-23 23:16:17 -0300

    Metodo ToString

--- ArrayList/ArrayCompany/src/models/employe.java
+++ ArrayList/ArrayCompany/src/models/employe.java
@@ -1,8 +1,43 @@
 package models;
 
 public class employe {
-	Integer id;
+	
 	String name;
 	Double salary;
+	public static  Integer id;
+	
+	public employe(String name, Double salary) {
+		
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String toString() 
+	{
+        return "ID " + id +", Name: " + name + ", Salary: " + salary ;
+
+	}
+	 
+	
+	
+	public static Integer getId() {
+		return id;
+	}
+	public static void setId(Integer id) {
+		employe.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
 
 }

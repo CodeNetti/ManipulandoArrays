4d287693ba6c69f7eaa27eb19fac45b01beb6e43
Raphael Simonetti <raphasimonettti@gmail.com>
2023-10-03 15:57:02 -0300

    Verificando Se Um Numero Existe Ou Não Na Matriz

---
+++ ArrayList/ProjectMatriz/src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("Olá vamos criar uma matriz");
+		System.out.println("Digite primeiro a quantidade de linhas e depois a quantidade de colunas");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite a quantidade de linhas");
+		int lines = sc.nextInt();
+		System.out.println("Digite a quantidade de colunas");
+		int columns = sc.nextInt();
+		int[][] matriz = new int[lines][columns];
+		for (int i = 0; i < columns; i++) {
+			for (int j = 0; j < lines; j++) {
+				matriz[j][i] = sc.nextInt();
+			}
+		}
+		boolean confirmation = false;
+		System.out.println(
+				"Agora vamos ver quais são os numereos a esquerda, direita, acima e abaixo de umm numero que você digitou na matriz");
+		System.out.println("Digite o numero");
+		int n = sc.nextInt();
+		for (int c = 0; c < columns; c++) {
+			for (int l = 0; l < lines; l++) {
+				if(matriz[l][c]  == n) 
+				{
+					if(columns > 1 && lines > 1) 
+					{
+					System.out.println("O número " + n + " foi encontrado na coluna  " + (c) + ",  na  linha " + (l) );
+						System.out.println("Esquerda" + matriz[1-c][l]);
+
+					}
+					
+					confirmation = true;
+					break;
+				}
+				else 
+				{
+					confirmation = false;
+				}
+			}
+		}
+		if(confirmation == false) 
+		{
+			System.out.println("Este numero não existe na matriz");
+		}
+		
+
+	}
+}
